9313962d643d74168c3eaec9d5d43101d9ef2cee
嘿嘿 <14479594+qi-ruolin@user.noreply.gitee.com>
2024-12-18 00:25:21 +0800

    new

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+import { fileURLToPath, URL } from 'node:url'
+ 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+ 
+export default defineConfig({
+  // publicPath:'./',
+  // outputDir:'dist',
+  // assetsDit: 'static',
+  plugins: [
+    vue(),
+  ],
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
