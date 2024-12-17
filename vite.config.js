f348d5ac725488755c9115352f3115fac6909aef
RuiQi <117133084+hiahei@users.noreply.github.com>
2024-12-18 00:03:16 +0800

    修改配置

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+import { fileURLToPath, URL } from 'node:url'
+ 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+ 
+export default defineConfig({
+  publicPath:'./',
+  outputDir:'./',
+  plugins: [
+    vue(),
+  ],
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
